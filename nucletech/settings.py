68bcbe0b547609afb1de713cc38a1509fbb92554
Karan Sawant <sawant.karan@siesgst.ac.in>
2019-07-20 01:24:41 +0530

    Settings Change

--- nucletech/settings.py
+++ nucletech/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
