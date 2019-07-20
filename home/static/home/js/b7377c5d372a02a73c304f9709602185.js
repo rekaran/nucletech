e5c8885cbce235b922095d336c23dd302b3ed7be
Karan Sawant <sawant.karan@siesgst.ac.in>
2019-07-20 09:06:54 +0530

    Initial Setup

--- home/static/home/js/b7377c5d372a02a73c304f9709602185.js
+++ home/static/home/js/b7377c5d372a02a73c304f9709602185.js
@@ -105,5 +105,12 @@ let startScript = () =>{
 // }
 startScript();
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://www.nuclechat.com/static/js/sw.js');
+    navigator.serviceWorker.register('https://www.nuclechat.com/static/js/sw.js')
+    .then(registration => {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, err => {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
 }
