206e1a5dc692706bec8e7ae84e47efe69dd91915
Karan Sawant <sawant.karan@siesgst.ac.in>
2019-07-21 21:38:54 +0530

    Lazy Load

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -1,1 +1,17 @@
 $(".side-nav-toggle").click(function(){$("#side-nav").css("width", "100vw");$(this).addClass("nav-hidden");$(".nav-icon-close").removeClass("nav-hidden");});$(".side-nav-content").click(function(e){e.stopPropagation();});$("#side-nav").click(function(){$(this).css("width", "0vw");$(".nav-icon-close").addClass("nav-hidden");$(".nav-icon-menu").removeClass("nav-hidden");});$(document).ready(function () {$(window).scroll(function() {var winOffset = document.documentElement.scrollTop || document.body.scrollTop;if(winOffset > 50) {$('.uk-navbar-container').addClass("nav-bar");} else {$('.uk-navbar-container').removeClass("nav-bar");}});});$(document).ready(function () {var winOffset = document.documentElement.scrollTop || document.body.scrollTop;if(winOffset > 50) {$('.uk-navbar-container').addClass("nav-bar");} else {$('.uk-navbar-container').removeClass("nav-bar");}});
+const config = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 0
+};
+let observer = new intersectionObserver(function(entries, self) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            preloadImage(entry.target);
+            self.unobserve(entry.target);
+        }
+    });
+}, config);
+const imgs = document.querySelectorAll('[data-src]');
+imgs.forEach(img => {
+    observer.observe(img);
+});
