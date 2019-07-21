4a726d14c17dc2d112544f94d57740cd8623d77c
Karan Sawant <sawant.karan@siesgst.ac.in>
2019-07-21 21:44:27 +0530

    Lazy Load

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -1,17 +1,74 @@
 $(".side-nav-toggle").click(function(){$("#side-nav").css("width", "100vw");$(this).addClass("nav-hidden");$(".nav-icon-close").removeClass("nav-hidden");});$(".side-nav-content").click(function(e){e.stopPropagation();});$("#side-nav").click(function(){$(this).css("width", "0vw");$(".nav-icon-close").addClass("nav-hidden");$(".nav-icon-menu").removeClass("nav-hidden");});$(document).ready(function () {$(window).scroll(function() {var winOffset = document.documentElement.scrollTop || document.body.scrollTop;if(winOffset > 50) {$('.uk-navbar-container').addClass("nav-bar");} else {$('.uk-navbar-container').removeClass("nav-bar");}});});$(document).ready(function () {var winOffset = document.documentElement.scrollTop || document.body.scrollTop;if(winOffset > 50) {$('.uk-navbar-container').addClass("nav-bar");} else {$('.uk-navbar-container').removeClass("nav-bar");}});
-const config = {
-    rootMargin: '0px 0px 50px 0px',
-    threshold: 0
-};
-let observer = new intersectionObserver(function(entries, self) {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            preloadImage(entry.target);
-            self.unobserve(entry.target);
+!function(window){
+    var $q = function(q, res){
+          if (document.querySelectorAll) {
+            res = document.querySelectorAll(q);
+          } else {
+            var d=document
+              , a=d.styleSheets[0] || d.createStyleSheet();
+            a.addRule(q,'f:b');
+            for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
+              l[b].currentStyle.f && c.push(l[b]);
+  
+            a.removeRule(0);
+            res = c;
+          }
+          return res;
         }
-    });
-}, config);
-const imgs = document.querySelectorAll('[data-src]');
-imgs.forEach(img => {
-    observer.observe(img);
-});
+      , addEventListener = function(evt, fn){
+          window.addEventListener
+            ? this.addEventListener(evt, fn, false)
+            : (window.attachEvent)
+              ? this.attachEvent('on' + evt, fn)
+              : this['on' + evt] = fn;
+        }
+      , _has = function(obj, key) {
+          return Object.prototype.hasOwnProperty.call(obj, key);
+        }
+      ;
+  
+    function loadImage (el, fn) {
+      var img = new Image()
+        , src = el.getAttribute('data-src');
+      img.onload = function() {
+        if (!! el.parent)
+          el.parent.replaceChild(img, el)
+        else
+          el.src = src;
+  
+        fn? fn() : null;
+      }
+      img.src = src;
+    }
+  
+    function elementInViewport(el) {
+      var rect = el.getBoundingClientRect()
+  
+      return (
+         rect.top    >= 0
+      && rect.left   >= 0
+      && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+      )
+    }
+  
+      var images = new Array()
+        , query = $q('img.lazy')
+        , processScroll = function(){
+            for (var i = 0; i < images.length; i++) {
+              if (elementInViewport(images[i])) {
+                loadImage(images[i], function () {
+                  images.splice(i, i);
+                });
+              }
+            };
+          }
+        ;
+      // Array.prototype.slice.call is not callable under our lovely IE8 
+      for (var i = 0; i < query.length; i++) {
+        images.push(query[i]);
+      };
+  
+      processScroll();
+      addEventListener('scroll',processScroll);
+  
+}(this);​
