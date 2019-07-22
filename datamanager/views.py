04194337317ea49c9db6b9296300469e101af256
Karan Sawant <sawant.karan@siesgst.ac.in>
2019-07-22 17:35:36 +0530

    Save Faq

--- datamanager/views.py
+++ datamanager/views.py
@@ -145,8 +145,8 @@ def savedata(request, name):
     # try:
     api_key = request.META["HTTP_TOKEN"]
     if request.method == "POST":
-        print(request.body.decode("utf-8"))
-        data = json.loads(request.body)
+        print(type request.body.decode("utf-8"))
+        data = json.loads(request.body.decode("utf-8"))
         profile = Profile.objects.get(api_key=api_key)
         if profile.user.email == request.user.email:
             flow_data = data["flow"]
