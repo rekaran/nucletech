6a541b8fc3d4909977e818038a4befeb54c7445c
Karan Sawant <sawant.karan@siesgst.ac.in>
2019-07-22 07:23:47 +0530

    Minified Jinja

--- home/views.py
+++ home/views.py
@@ -1,31 +1,31 @@
 from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.contrib.auth.decorators import login_required
-from htmlmin.decorators import minified_response
+# from htmlmin.decorators import minified_response
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 
 # Create your views here.
-@minified_response
+# @minified_response
 def index(request):
     return render(request, 'home/index.html')
 
 
-@minified_response
+# @minified_response
 def contact(request):
     return render(request, 'home/contact.html')
 
 
-@minified_response
+# @minified_response
 def about(request):
     return render(request, 'home/about.html')
 
 
-@minified_response
+# @minified_response
 def commingsoon(request):
     return render(request, 'home/comming.html')
 
 
-@minified_response
+# @minified_response
 def register(request):
     if request.method=="GET":
         form = SignUpForm()
