50cdeab2cd987626bba395a22fb9840d94b8cbb8
Karan Sawant <sawant.karan@siesgst.ac.in>
2019-07-24 13:59:37 +0530

    Settings

--- datamanager/views.py
+++ datamanager/views.py
@@ -140,6 +140,7 @@ def getdata(request, name):
                     print(data_keys, project_id, project_hash)
                     del data_keys["_id"]
                 except Exception as e:
+                    print(e)
                     data_keys = {}
                 return JsonResponse({"success": 200, "flow": flow_data["data"], "faq": faq_data["data"], "smalltalk": st_data["data"], "datamatrix": dict(data_martix), "keymapper": dict(data_keys)}, safe=False)
         raise Http404
