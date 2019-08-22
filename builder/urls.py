cd6df126150632be58743e4c272880205388902d
Karan Sawant <sawant.karan@siesgst.ac.in>
2019-08-22 13:18:30 +0530

    Profile

--- builder/urls.py
+++ builder/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', login_required(views.index), name='builder.index'),
     url(r'^(?P<name>[\w-]+)/$', login_required(views.edit), name='builder.edit'),
-    url(r'^profile/$', login_required(views.edit), name='builder.profile'),
+    url(r'^profile/$', login_required(views.profile), name='builder.profile'),
     # url(r"^register/$", views.register, name="home.register"),
 ]
